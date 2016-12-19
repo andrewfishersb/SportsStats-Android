4ae3d2a1cfd12c4219b4c16096512449a49b1538
Andrew Fisher <andrewfishersb@gmail.com>
2016-12-19 09:31:41 -0800

    create a player activity, create a team activity and models for both players and teams

---
+++ app/src/main/java/fisher/andrew/sportstats/Constants.java
@@ -0,0 +1,7 @@
+package fisher.andrew.sportstats;
+
+/**
+ * Created by Guest on 12/19/16.
+ */
+public class Constants {
+}
---
+++ app/src/main/java/fisher/andrew/sportstats/model/Player.java
@@ -0,0 +1,7 @@
+package fisher.andrew.sportstats.model;
+
+/**
+ * Created by Guest on 12/19/16.
+ */
+public class Player {
+}

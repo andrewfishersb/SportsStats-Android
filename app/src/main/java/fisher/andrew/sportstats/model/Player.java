5ac46d8b6393b4b4ca9b0b90508513ac213420a6
Andrew Fisher <andrewfishersb@gmail.com>
2016-12-19 09:55:21 -0800

    create attributes of players plus methods that will add to the players stat attributes, and display all player information

--- app/src/main/java/fisher/andrew/sportstats/model/Player.java
+++ app/src/main/java/fisher/andrew/sportstats/model/Player.java
@@ -1,7 +1,120 @@
 package fisher.andrew.sportstats.model;
 
-/**
- * Created by Guest on 12/19/16.
- */
+import org.parceler.Parcel;
+
+@Parcel
 public class Player {
+
+
+    String name;
+    String height;
+    String age;
+    int freeThrows;
+    //a regular 2 pt basket
+    int fieldGoals;
+    int threePointers;
+    int assists;
+    int blocks;
+    int steals;
+    int totalPoints;
+
+    public Player(String name, String height, String age){
+        this.name = name;
+        this.height = height;
+        this.age = age;
+        this.freeThrows=0;
+        this.fieldGoals=0;
+        this.threePointers=0;
+        this.assists=0;
+        this.blocks=0;
+        this.steals=0;
+        this.totalPoints =0 ;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public int getFreeThrows() {
+        return freeThrows;
+    }
+
+    public void addFreeThrow(int freeThrows) {
+        this.freeThrows = freeThrows;
+    }
+
+    public int getFieldGoals() {
+        return fieldGoals;
+    }
+
+    public void addFieldGoal(int fieldGoals) {
+        this.fieldGoals++;
+    }
+
+    public int getThreePointers() {
+        return threePointers;
+    }
+
+    public void addThreePointer() {
+        this.threePointers++;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void addAssist() {
+        this.assists++;
+    }
+
+    public int getBlocks() {
+        return blocks;
+    }
+
+    public void addBlock() {
+        this.blocks++;
+    }
+
+    public int getSteals() {
+        return steals;
+    }
+
+    public void addSteal() {
+        this.steals++;
+    }
+    public int setTotalPoints(int threePointers, int fieldGoals, int freeThrows){
+        this.totalPoints = freeThrows + (2*fieldGoals) + (3*threePointers);
+    }
+    public int getTotalPoints(){
+        return this.totalPoints;
+    }
+
 }
+// ---ADD LATER IF TIME---
+//more stats
+//rank players
+//personal information, age, height, weight? dob
+//eventually have overall stats versus current game stats
+//refractor add methods;
+//set methods once game is over to change the players overall stats
+//export as csv file
--- app/src/main/java/fisher/andrew/sportstats/model/Team.java
+++ app/src/main/java/fisher/andrew/sportstats/model/Team.java
@@ -1,16 +1,20 @@
 package fisher.andrew.sportstats.model;
 
+import org.parceler.Parcel;
+
 import java.util.ArrayList;
 
-
+@Parcel
 public class Team {
 
     private String name;
     ArrayList<Player> players;
+    String index;
 
     public Team(String name, ArrayList<Player> players) {
         this.name = name;
         this.players = players;
+        this.index = "none_specified";
     }
 
     public String getName() {
@@ -23,6 +27,14 @@ public class Team {
 
     public void addPlayer(Player newPlayer){
         players.add(newPlayer);
+    }
+
+    public String getIndex(){
+        return index;
+    }
+
+    public void setIndex(String index){
+        this.index=index;
     }
 }
 //add index after generate

974a0b2c3a6331fd1bb6a07151f895a4ba0aacde
Andrew Fisher <AndrewFishersb@gmail.com>
2016-12-19 16:32:10 -0800

    pass an id for the view, the index of player clicked and the array of players

--- app/src/main/java/fisher/andrew/sportstats/adapter/FirebasePlayerViewHolder.java
+++ app/src/main/java/fisher/andrew/sportstats/adapter/FirebasePlayerViewHolder.java
@@ -2,6 +2,7 @@ package fisher.andrew.sportstats.adapter;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -9,12 +10,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import fisher.andrew.sportstats.Constants;
 import fisher.andrew.sportstats.R;
 import fisher.andrew.sportstats.model.Player;
+import fisher.andrew.sportstats.ui.TrackStatActivity;
 
 public class FirebasePlayerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-
+    TextView fieldGoalsTextView;
 
     View mView;
     Context mContext;
@@ -30,7 +43,7 @@ public class FirebasePlayerViewHolder extends RecyclerView.ViewHolder implements
     public void bindPlayer(Player player){
 
         TextView nameTextView = (TextView) mView.findViewById(R.id.playerName);
-        TextView fieldGoalsTextView= (TextView) mView.findViewById(R.id.playerFG);
+         fieldGoalsTextView= (TextView) mView.findViewById(R.id.playerFG);
         TextView threePointsTextView= (TextView) mView.findViewById(R.id.player3Pts);
         TextView freeThrowsTextView = (TextView) mView.findViewById(R.id.playerFT);
         TextView reboundsTextView = (TextView) mView.findViewById(R.id.playerReb);
@@ -49,10 +62,38 @@ public class FirebasePlayerViewHolder extends RecyclerView.ViewHolder implements
         blocksTextView.setText(player.getBlocks()+"");
         totalPointsTextView.setText(player.getTotalPoints()+"");
 
+
+
+        fieldGoalsTextView.setOnClickListener(this);
     }
 
+    //possibly click on the item but will then send the id, position and the intent
     @Override
-    public void onClick(View view){
+    public void onClick(final View view){
+        final ArrayList<Player> players = new ArrayList<>();
+        final int viewId = view.getId();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PLAYER);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    players.add(snapshot.getValue(Player.class));
+                }
+                int playerIndex = getLayoutPosition();
+                Intent intent = new Intent(mContext, TrackStatActivity.class);
+                intent.putExtra("view_id",viewId);
+                intent.putExtra("index", playerIndex + "");
+                intent.putExtra("restaurants", Parcels.wrap(players));
+
+                mContext.startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
--- app/src/main/java/fisher/andrew/sportstats/ui/TrackStatActivity.java
+++ app/src/main/java/fisher/andrew/sportstats/ui/TrackStatActivity.java
@@ -1,5 +1,6 @@
 package fisher.andrew.sportstats.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,8 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import fisher.andrew.sportstats.Constants;
@@ -16,9 +19,17 @@ import fisher.andrew.sportstats.R;
 import fisher.andrew.sportstats.adapter.FirebasePlayerViewHolder;
 import fisher.andrew.sportstats.model.Player;
 
+//notes
+//from intent grab the 3 items, use the array and index to find who they clicked then use an id to compare which area clicked and update firebase
+
 public class TrackStatActivity extends AppCompatActivity {
     private FirebaseRecyclerAdapter mFirebaseAdapter;
     private DatabaseReference mPlayerReference;
+
+    //intent variables
+    private int viewId;
+    private ArrayList<Player> players;
+    private int playersIndex;
 
     @Bind(R.id.playerStatRecyclerView) RecyclerView mRecyclerView;
     @Override
@@ -30,7 +41,20 @@ public class TrackStatActivity extends AppCompatActivity {
 
 
         mPlayerReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PLAYER);
+
+        if(getIntent()!=null){
+            Intent intent = getIntent();
+
+
+
+
+        }
+
+
+
         setUpFirebaseAdapter();
+
+
 
     }
 

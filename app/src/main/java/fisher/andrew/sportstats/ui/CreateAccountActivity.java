64181feda8c6e57a01ba44ccde1584b1c7ec7baf
Andrew Fisher <AndrewFishersb@gmail.com>
2017-01-06 15:29:44 -0800

    create an account page layout

---
+++ app/src/main/java/fisher/andrew/sportstats/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package fisher.andrew.sportstats.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fisher.andrew.sportstats.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}

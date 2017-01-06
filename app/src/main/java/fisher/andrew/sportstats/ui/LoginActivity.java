5f5704597672ed1f152ed7d8946c4344b8c0e301
Andrew Fisher <AndrewFishersb@gmail.com>
2017-01-06 15:47:43 -0800

    Change app to launch to the login page. and link to the create an account page if they click that line

--- app/src/main/java/fisher/andrew/sportstats/ui/LoginActivity.java
+++ app/src/main/java/fisher/andrew/sportstats/ui/LoginActivity.java
@@ -1,13 +1,41 @@
 package fisher.andrew.sportstats.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import fisher.andrew.sportstats.R;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.loginEmail) EditText mLoginEmail;
+    @Bind(R.id.loginPassword) EditText mLoginPassword;
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.registerTextView) TextView mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+//        mLoginButton.setOnClickListener(this);
+        mSignUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view ==mSignUp){
+            Intent intent = new Intent(LoginActivity.this,CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }

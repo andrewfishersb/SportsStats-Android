c8d866f2eedc247f9183d4d52d40537aa986e275
Andrew Fisher <AndrewFishersb@gmail.com>
2017-01-06 16:11:32 -0800

    start implementing firebase to create a new user

--- app/src/main/java/fisher/andrew/sportstats/ui/CreateAccountActivity.java
+++ app/src/main/java/fisher/andrew/sportstats/ui/CreateAccountActivity.java
@@ -1,15 +1,106 @@
 package fisher.andrew.sportstats.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import fisher.andrew.sportstats.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.createEmail) EditText mCreateEmail;
+    @Bind(R.id.createPassword) EditText mCreatePass;
+    @Bind(R.id.confirmPassword) EditText mConfirmPass;
+    @Bind(R.id.createAccountButton) Button mCreateAccountButton;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+
+    //user authentication
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+
+        mCreateAccountButton.setOnClickListener(this);
+        mLoginTextView.setOnClickListener(this);
+
     }
+
+    public void onClick(View v){
+        if(v == mLoginTextView){
+            Intent intent = new Intent(CreateAccountActivity.this,LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if(v ==mCreateAccountButton){
+            createUser();
+        }
+    }
+
+    private void createAuthStateListener(){
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    public void createUser(){
+        final String email = mCreateEmail.getText().toString().trim();
+        String password = mCreatePass.getText().toString().trim();
+        String confirmPassword = mConfirmPass.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                }else{
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
 }
